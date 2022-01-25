b217127f852a8086b19b67ac2c04f314800a2a6f
Alireza <asg.1371@gmail.com>
2022-01-26 00:52:46 +0330

    Pushed!

---
+++ app/src/main/java/com/example/tictactoegame/Player.kt
@@ -0,0 +1,15 @@
+package com.example.tictactoegame
+
+import android.graphics.Color
+import android.view.View
+
+class Player {
+    fun click(layout: View){
+
+
+
+
+    }
+    fun setColor(color:Color){
+    }
+}
